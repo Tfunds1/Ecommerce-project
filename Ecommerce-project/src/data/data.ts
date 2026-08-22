fa3286ec3c05d7c709d5f8102292bcd3d364fd85
Rotimi-Ayeni <tomisinayeni020@gmail.com>
2026-08-22 16:18:03 +0100

    Fix asset import paths in data.ts after moving data folder

--- Ecommerce-project/src/data/data.ts
+++ Ecommerce-project/src/data/data.ts
@@ -1,14 +1,14 @@
-import menImg from "../../assets/category1/men.png";
-import womenImg from "../../assets/category1/woman.png";
-import babyToddlerCard from "../../assets/category1/baby-toddler.png";
-import beautyImg from "../../assets/category1/beauty.png";
-import healthImg from "../../assets/category1/health-wellness.png";
+import menImg from "../assets/category1/men.png";
+import womenImg from "../assets/category1/woman.png";
+import babyToddlerCard from "../assets/category1/baby-toddler.png";
+import beautyImg from "../assets/category1/beauty.png";
+import healthImg from "../assets/category1/health-wellness.png";
 
-import menImg2 from "../../assets/category2/men's wear img.png";
-import womenImg2 from "../../assets/category2/women's wear img.png";
-import babyToddlerImg2 from "../../assets/category2/baby's wear img.png";
-import beautyImg2 from "../../assets/category2/beauty img.png";
-import healthImg2 from "../../assets/category2/health img.png";
+import menImg2 from "../assets/category2/men's wear img.png";
+import womenImg2 from "../assets/category2/women's wear img.png";
+import babyToddlerImg2 from "../assets/category2/baby's wear img.png";
+import beautyImg2 from "../assets/category2/beauty img.png";
+import healthImg2 from "../assets/category2/health img.png";
 
 export type Category = {
   label: string;
